befc0b9ddf03b1963065981cc241f0ae4249fd62
Jinni Xia <Jinni Xia>
2017-01-23 16:18:16 -0500

    Filled out calc method for Person1

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		String s = input.substring(2, input.length);
+		s = s + input.charAt(0) + input.charAt(1);
+	  return s;
 	}
 	
 	/**
